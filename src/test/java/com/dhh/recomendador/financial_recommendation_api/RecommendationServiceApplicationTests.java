26fc2c55caf31f79bcbc653dc5d375ba5d338478
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-06-07 16:26:36 +0200

    first commit

---
+++ src/test/java/com/dhh/recomendador/financial_recommendation_api/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhh.recomendador.financial_recommendation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
