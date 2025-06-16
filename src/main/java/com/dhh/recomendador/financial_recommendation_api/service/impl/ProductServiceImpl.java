97e7b549a607e7067c12154191c90d191472d224
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-06-16 10:16:21 +0200

    añadido embedding producto

--- src/main/java/com/dhh/recomendador/financial_recommendation_api/service/impl/ProductServiceImpl.java
+++ src/main/java/com/dhh/recomendador/financial_recommendation_api/service/impl/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.dhh.recomendador.financial_recommendation_api.mapper.ProductMapper;
 import com.dhh.recomendador.financial_recommendation_api.model.Product;
 import com.dhh.recomendador.financial_recommendation_api.model.dto.ProductDTO;
 import com.dhh.recomendador.financial_recommendation_api.repository.ProductRepository;
+import com.dhh.recomendador.financial_recommendation_api.service.EmbeddingService;
 import com.dhh.recomendador.financial_recommendation_api.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +22,14 @@ public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final EmbeddingService embeddingService;
 
     @Override
     public ProductDTO createProduct(ProductDTO productDTO) {
         log.info("Se guarda el producto: {}", productDTO.toString());
         Product entity = productMapper.toEntity(productDTO);
         Product saved = productRepository.save(entity);
+        saved.setEmbedding(embeddingService.buildProductText(saved));
         return productMapper.toDto(saved);
     }
 
@@ -52,6 +55,7 @@ public class ProductServiceImpl implements ProductService {
                 .map(existing -> {
                     Product entity = productMapper.toEntity(updatedProduct);
                     entity.setId(id);
+                    entity.setEmbedding(embeddingService.buildProductText(entity));
                     return productRepository.save(entity);
                 })
                 .map(productMapper::toDto)
